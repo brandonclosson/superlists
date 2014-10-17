136297eb51234745c31579993c57cb90ebe37b48
brandonclosson <bclosson@gmail.com>
2014-10-17 11:36:02 -0400

    quick fix to FT

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,8 @@ class NewVisitorTest(unittest.TestCase):
 		self.browser.quit()
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+
 		# User notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
 		self.fail('Finish the tests!')
