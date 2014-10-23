22feae2404646c50f4b068d40d0182cbcca119f9
brandonclosson <bclosson@gmail.com>
2014-10-23 16:58:42 -0400

    added javascript testing with qunit

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -2,6 +2,9 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
     
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         
         # User goes to home page and accidentally tries to submit an empty list item
@@ -9,7 +12,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('\n')
         
         # Home page refrehes and there is an error message saying it can't be blank
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # User tries again with some text and it works
@@ -20,7 +23,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('\n')
 
         # User receives similar warning on their list
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # User corrects it by filling in text
@@ -36,8 +39,22 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Buy wellies\n')
 
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        # User starts a new list that causes a validation error
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # User starts typing to clear error message
+        self.get_item_input_box().send_keys('a')
+
+        # Error message is gone
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
